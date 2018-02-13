3c7e3b054f109adbe2556ed5435eab32a74c554b
Leandro Beretta <lea.beretta@gmail.com>
2018-02-13 11:26:30 -0300

    se inicia el login (#8)

--- openscore-core/src/test/java/io/semantic/openscore/core/repository/UsuarioRepositoryTest.java
+++ openscore-core/src/test/java/io/semantic/openscore/core/repository/UsuarioRepositoryTest.java
@@ -1,5 +1,49 @@
 package io.semantic.openscore.core.repository;
 
-class UsuarioRepositoryTest {
+import io.semantic.openscore.core.model.Pais;
+import io.semantic.openscore.core.model.Usuario;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+public class UsuarioRepositoryTest extends RepositoryBaseTest {
+
+    private UsuarioRepository repository;
+    private Logger logger = LoggerFactory.getLogger(RepositoryTest.class);
+
+    @Test
+    public void testFindByEmail() {
+        String email = "john.frusciante@gmail.com";
+
+        Usuario newUser = new Usuario();
+
+        newUser.setNombre("John");
+        newUser.setApellido("Frusciante");
+        newUser.setEmail(email);
+        newUser.setPassword("anthony_kiedis");
+
+        Pais pais = new Pais();
+        pais.setNombre("Argentina");
+        pais.setCodigo("ARG");
+
+        PaisRepository paisRepository = new PaisRepository(this.entityManager);
+        transaction(() -> paisRepository.save(pais));
+
+        newUser.setPais(pais);
+
+        transaction(() -> this.repository.save(newUser));
+
+        Optional<Usuario> user = this.repository.findByEmail(email);
+
+        assertEquals(email, user.get().getEmail());
+    }
+
+    @Override
+    protected void beforeEach() {
+        this.repository = new UsuarioRepository(this.entityManager);
+    }
 }
