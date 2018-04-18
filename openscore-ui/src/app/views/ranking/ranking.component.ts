66104ffcaf518286dc9cf62635b6181400b6aa3a
Adriel Paredes <adriel.paredes@gmail.com>
2018-04-18 18:01:41 -0300

    se mejora el ranking

--- openscore-ui/src/app/views/ranking/ranking.component.ts
+++ openscore-ui/src/app/views/ranking/ranking.component.ts
@@ -1,6 +1,7 @@
 import { RankingService } from './../../services/ranking.service';
 import { Component, OnInit } from '@angular/core';
 import { Ranking } from '../../model/ranking';
+import { INTERNAL_BROWSER_PLATFORM_PROVIDERS } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-ranking',
@@ -22,6 +23,19 @@ export class RankingComponent implements OnInit {
       res => {
         this.ranking = res.data;
       });
+  }
+
+  getCopa(ranking: Ranking) {
+    console.log(ranking.ranking);
+    if (ranking.ranking === 1) {
+      return 'fas fa-trophy gold';
+    } else if (ranking.ranking === 2) {
+      return 'fas fa-trophy silver';
+    } else if (ranking.ranking === 3) {
+      return 'fas fa-trophy bronze';
+    } else {
+      return '';
+    }
   }
 
 }
