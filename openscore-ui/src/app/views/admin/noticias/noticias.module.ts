0df2c001217a42b4ff04b118279dfa2f0cfc1909
Adriel Paredes <adriel.paredes@gmail.com>
2018-05-07 23:36:28 -0300

    #17 #20: Se agregan los toastr para las noticias. Se agrega el componente de toastr

--- openscore-ui/src/app/views/admin/noticias/noticias.module.ts
+++ openscore-ui/src/app/views/admin/noticias/noticias.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoticiasRoutes } from './noticias.routing';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
@@ -6,15 +7,14 @@ import { NoticiasComponent } from './noticias.component';
 import { NgxEditorModule } from 'ngx-editor';
 import { NoticiasEditorComponent } from './noticias-editor/noticias-editor.component';
 import { NoticiasListComponent } from './noticias-list/noticias-list.component';
-import { ToastModule } from 'ng2-toastr/ng2-toastr';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   imports: [
     CommonModule,
     NgxEditorModule,
     FormsModule,
-    NoticiasRoutes,
-    ToastModule.forRoot()
+    NoticiasRoutes
   ],
   declarations: [NoticiasComponent,
     NoticiasEditorComponent,
