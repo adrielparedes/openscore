91b0362ce4cf27d932ed0d8ba9f22be56dce34f9
Adriel Paredes <adriel.paredes@gmail.com>
2018-06-04 15:34:12 -0300

    Se arregla el dia builder

--- openscore-core/src/main/java/io/semantic/openscore/core/repository/startup/builder/DiaBuilderImpl.java
+++ openscore-core/src/main/java/io/semantic/openscore/core/repository/startup/builder/DiaBuilderImpl.java
@@ -6,10 +6,16 @@ import java.util.Date;
 
 public class DiaBuilderImpl implements DiaBuilder {
 
+    private SimpleDateFormat simpleDateFormat;
+
+    public DiaBuilderImpl() {
+        simpleDateFormat = new SimpleDateFormat(DATE_PATTERN);
+    }
+
     public Date getMatchDate(String fecha) {
 
         try {
-            return new SimpleDateFormat().parse(fecha);
+            return simpleDateFormat.parse(fecha);
         } catch (ParseException e) {
             e.printStackTrace();
             return null;
