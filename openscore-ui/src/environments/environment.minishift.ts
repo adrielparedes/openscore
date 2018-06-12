a0b6db166ffca3a7de097d54918982abef717006
Leandro Beretta <lea.beretta@gmail.com>
2018-06-12 20:15:31 -0300

    varios cambios estéticos

--- openscore-ui/src/environments/environment.minishift.ts
+++ openscore-ui/src/environments/environment.minishift.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     name: 'test',
     version: require('../../package.json').version,
-    url: 'http://openscore-core-openscore.127.0.0.1.nip.io/openscore/api/rest'
+    url: 'http://openscore-core-openscore.192.168.64.37.nip.io/openscore/api/rest'
 };
