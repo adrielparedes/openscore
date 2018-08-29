6fa39da03a5acd5de1b25dd2139874830d42c504
Adriel <adriel.paredes@gmail.com>
2018-08-29 15:41:59 -0300

    Se cambian algunas cosas mas

--- openscore-openshift/create.sh
+++ openscore-openshift/create.sh
@@ -15,3 +15,12 @@ oc new-app $REPO#$BRANCH --context-dir=openscore-ui --strategy=pipeline --name="
 oc set env bc/openscore-ui-pipeline REPO=$REPO -n openscore
 oc set env bc/openscore-ui-pipeline BRANCH=$BRANCH -n openscore
 
+oc create configmap openscore-core-config --from-file=../openscore-core/src/main/resources/configmap.properties
+
+oc create configmap openscore-core-config \
+    --from-literal=POSTGRESQL_DATABASE=openscore \
+    --from-literal=POSTGRESQL_SERVICE_PORT=5432 \
+    --from-literal=POSTGRESQL_SERVICE_HOST=openscore-db \
+    --from-literal=POSTGRESQL_PASSWORD=redhat01 \
+    --from-literal=POSTGRESQL_USER=admin \
+    --from-literal=POSTGRESQL_DATASOURCE=openscore
