1bb5fda040d3eda4a3c020151aaeb28f376ac66d
Adriel Paredes <adriel.paredes@gmail.com>
2019-06-09 09:17:27 -0300

    Se agrega el twitter de la copa america al home

--- openscore-ui/src/app/views/home/home.module.ts
+++ openscore-ui/src/app/views/home/home.module.ts
@@ -2,11 +2,13 @@ import { HomeRoutes } from './home.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { NgxTwitterTimelineModule } from 'ngx-twitter-timeline';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutes
+    HomeRoutes,
+    NgxTwitterTimelineModule
   ],
   declarations: [HomeComponent]
 })
