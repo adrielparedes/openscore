a8f99b435fa7c3844f32572a3b8bceb330272c1b
Adriel Paredes <adriel.paredes@gmail.com>
2021-06-10 17:14:53 -0300

    Dockerfile para apache y UI

--- openscore-ui/src/environments/environment.prod.ts
+++ openscore-ui/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   name: 'prod',
   version: require('../../package.json').version,
-  url: 'http://openscore-core-openscore.192.168.42.175.nip.io/openscore/api/rest'
+  url: 'http://www.rhopenscore.com/openscore/api/rest'
 };
