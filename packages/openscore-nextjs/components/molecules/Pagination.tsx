d5c806393bdd054b7d0ba02daf5f7f718038ec1b
Adriel Paredes <adriel.paredes@gmail.com>
2022-10-12 10:15:41 -0300

    Se mejora la paginacion

--- packages/openscore-nextjs/components/molecules/Pagination.tsx
+++ packages/openscore-nextjs/components/molecules/Pagination.tsx
@@ -10,20 +10,30 @@ const Pagination = ({ page, setPage, pageSize, count }: PaginationProps) => {
   return (
     <nav aria-label="Page navigation example">
       <ul className="pagination">
-        <li className="page-item">
-          <a className="page-link" href="#">
+        <li className={`page-item ${page === 0 ? "disabled" : ""}`}>
+          <a
+            className="page-link"
+            onClick={() => {
+              if (page > 0) setPage(page - 1);
+            }}
+          >
             Previous
           </a>
         </li>
         {Array.from(Array(pages).keys()).map((key) => (
-          <li className="page-item" key={key}>
+          <li className={`page-item ${page === key ? "active" : ""}`} key={key}>
             <a className="page-link" onClick={() => setPage(key)}>
               {key + 1}
             </a>
           </li>
         ))}
-        <li className="page-item">
-          <a className="page-link" href="#">
+        <li className={`page-item ${page === pages - 1 ? "disabled" : ""}`}>
+          <a
+            className="page-link"
+            onClick={() => {
+              if (page < pages - 1) setPage(page + 1);
+            }}
+          >
             Next
           </a>
         </li>
