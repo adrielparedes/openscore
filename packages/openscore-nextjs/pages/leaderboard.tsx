18772fd60ef8727dcf5d234a70313c6f51c007e6
Adriel Paredes <adriel.paredes@gmail.com>
2022-10-21 17:26:54 -0300

    Se anexa el servicio de rankings y se agrega un filtro rudimentario

--- packages/openscore-nextjs/pages/leaderboard.tsx
+++ packages/openscore-nextjs/pages/leaderboard.tsx
@@ -1,75 +1,105 @@
-import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import EmptyScreen from "../components/molecules/EmptyScreen";
+import LoadingScreen from "../components/molecules/LoadingScreen";
 import { layout } from "../components/templates/MainLayout";
+import { Ranking } from "../model/Ranking";
+import { RankingService } from "../services/RankingService";
 import { NextPageWithLayout } from "./_app";
 
-const leaderboard = [
-  {
-    name: "Adriel Paredes",
-    score: 71,
-    country: "Argentina",
-    position: 1,
-  },
-  {
-    name: "Leandro Beretta",
-    score: 70,
-    country: "Brasil",
-    position: 2,
-  },
-  {
-    name: "Emilia Paredes",
-    score: 69,
-    country: "Peru",
-    position: 3,
-  },
-];
-
 const Leaderboard: NextPageWithLayout = () => {
+  const [leaderboard, setLeaderboard] = useState<Ranking[]>([]);
+  const [filteredLeaderboard, setFilteredLeaderboard] = useState<Ranking[]>([]);
+  const [filter, setFilter] = useState("");
+  const [busy, setBusy] = useState(false);
+  const service = new RankingService();
+
+  useEffect(() => {
+    setBusy(true);
+    setFilter("");
+    service
+      .getAll("", 1)
+      .then((res) => {
+        setLeaderboard(res.data.data);
+        setFilteredLeaderboard(res.data.data);
+        setBusy(false);
+      })
+      .catch((err) => {
+        setLeaderboard([]);
+        setFilteredLeaderboard([]);
+        setBusy(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (filter !== undefined && filter.length > 0) {
+      setFilteredLeaderboard(
+        leaderboard.filter((l) => l.nombre.includes(filter))
+      );
+    } else {
+      setFilteredLeaderboard(leaderboard);
+    }
+  }, [filter]);
+
   return (
     <div>
       <h1>Leaderboard</h1>
 
-      <div className="top__three mb-4">
-        <h5>Top 3</h5>
-        <div className="leaderboard">
-          {leaderboard.map((l) => (
-            <div key={l.name} className="card border-light leaderboard__line">
-              <div className="card-body ">
-                <div className="leaderboard__left">
-                  <div className="leaderboard__name">{l.name}</div>
-                  <div className="leaderboard__score">
-                    Score: {l.score} points
-                  </div>
-                </div>
-                <div className="leaderboard__country">{l.country}</div>
+      <LoadingScreen busy={busy}>
+        <EmptyScreen isEmpty={leaderboard.length < 1}>
+          <div className="top__three mb-4">
+            <h5>Top 3</h5>
+            <div className="leaderboard">
+              {filteredLeaderboard.map((l) => (
                 <div
-                  className={`leaderboard__position leaderboard__position--${l.position}`}
+                  key={l.nombre}
+                  className="card border-light leaderboard__line"
                 >
-                  <span>{l.position}</span>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="others">
-        <h5>Others</h5>
-        <div className="leaderboard">
-          {leaderboard.map((l) => (
-            <div key={l.name} className="card border-light leaderboard__line">
-              <div className="card-body ">
-                <div className="leaderboard__left">
-                  <div className="leaderboard__name">{l.name}</div>
-                  <div className="leaderboard__score">
-                    Score: {l.score} points
+                  <div className="card-body ">
+                    <div className="leaderboard__left">
+                      <div className="leaderboard__nombre">{l.nombre}</div>
+                      <div className="leaderboard__puntos">
+                        Score: {l.puntos} points
+                      </div>
+                    </div>
+                    <div className="leaderboard__country">{l.pais}</div>
+                    <div
+                      className={`leaderboard__position leaderboard__position--${l.ranking}`}
+                    >
+                      <span>{l.ranking}</span>
+                    </div>
                   </div>
                 </div>
-                <div className="leaderboard__country">{l.country}</div>
-                <div className="leaderboard__position">{l.position}</div>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+          <div className="others">
+            <h5>Others</h5>
+            <div className="leaderboard">
+              <input
+                placeholder="Search..."
+                onChange={(content) => setFilter(content.target.value)}
+              ></input>
+              {filteredLeaderboard.map((l) => (
+                <div
+                  key={l.nombre}
+                  className="card border-light leaderboard__line"
+                >
+                  <div className="card-body ">
+                    <div className="leaderboard__left">
+                      <div className="leaderboard__nombre">{l.nombre}</div>
+                      <div className="leaderboard__puntos">
+                        Score: {l.puntos} points
+                      </div>
+                    </div>
+                    <div className="leaderboard__country">{l.pais}</div>
+                    <div className="leaderboard__position">{l.ranking}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </EmptyScreen>
+      </LoadingScreen>
     </div>
   );
 };
